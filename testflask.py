b6e27e322b57a4641e190d7a09615f50c74b7c29
Pedrat <po.matrix@gmail.com>
2018-02-19 11:16:10 +0000

    Olá Catia

---
+++ testflask.py
@@ -0,0 +1,55 @@
+import os
+from flask import session,escape,Flask,url_for,render_template,request,redirect
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = '/home/mint/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return '(/logout para saires) Logged in as %s' % escape(session['username'])
+    return 'You are not logged in(Vai para /login)'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+app.secret_key = '\xdc\x12\xdb\xd9\x07Y\x9c\x7f=\xd2\xd5*:\x82B/\xe9OZ\x91d\xd2\x9dR'
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',filename=filename))
+    return render_template('upload.html')
